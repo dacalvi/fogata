84177789cb4f287aa93fd2a5a08213a6be84e09a
Daniel Calvi <dacalvi@gmail.com>
2018-10-04 17:28:03 -0300

    Codeigniter Added, HMVC Added, Frontend and Backend Controllers Added

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,25 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+class CommonController extends MX_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+class FrontendController extends CommonController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+class BackendController extends CommonController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
